a6d7fe8bb331f5b0e1709e3dc40ed312119181bd
cfddream <cfddream@gmail.com>
2012-05-30 14:09:23 +0800

    fixed bug

--- js/jquery.ui.lastfocus.js
+++ js/jquery.ui.lastfocus.js
@@ -3,12 +3,14 @@
 
   $.fn.lastfocus = $.browser.mozilla ?
     function () {
+      if (!this[0]) return;
       var target = this[0]
         , l = target.value.length;
       target.focus();
       target.setSelectionRange(l, l);
     } :
     function () {
+      if (!this[0]) return;
       this[0].focus();
     };
 
