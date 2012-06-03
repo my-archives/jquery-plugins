f546129a149728174701e104f065559638474eda
cfddream <cfddream@gmail.com>
2012-06-04 01:12:23 +0800

    improved

--- js/jquery.ui.lastfocus.js
+++ js/jquery.ui.lastfocus.js
@@ -1,7 +1,7 @@
 !function ( $ ) {
   // fixed Mozilla Firefox/Opera focus
 
-  $.fn.lastfocus = ($.browser.mozilla || $.browser.opera) ?
+  $.fn.lastfocus = ($.browser.mozilla | $.browser.opera) ?
     function () {
       if (!this[0]) return;
       var target = this[0]
