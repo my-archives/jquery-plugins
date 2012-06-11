180e789c6fcbc0e64ee4ee154764542dae3f0138
cfddream <cfddream@gmail.com>
2012-06-12 01:59:51 +0800

    renamed focusend

---
+++ js/jquery.ui.dragleave.js
@@ -0,0 +1,40 @@
+!function ( $ ) {
+
+  /**
+   * Refer:
+   *    http://dev.w3.org/html5/spec/dnd.html
+   *    http://stackoverflow.com/questions/10253663/how-to-detect-the-dragleave-event-in-firefox-when-dragging-outside-the-window
+   *    http://jsfiddle.net/robertc/HU6Mk/3/
+   */
+
+  $.event.special.dragleave = {
+
+    delegateType: 'dragleave',
+
+    bindType: 'dragleave',
+
+    handle: function (e) {
+
+      var target = this,
+          related = e.relatedTarget,
+          handleObj = e.handleObj,
+          inside = false,
+          ret;
+
+      if (related !== target) {
+
+        if (related) {
+          inside = $.contains(target, related);
+        }
+
+        if (!inside) {
+          ret = handleObj.handler.apply(target, arguments);
+          e.type = 'dragleave';
+        }
+      }
+
+      return ret;
+    }
+  };
+
+}( jQuery );
